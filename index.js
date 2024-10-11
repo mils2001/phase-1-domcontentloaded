5662190615b62d53fbb4f730c1372c109bee08a1
chris <Mileschris466@gmail.com>
2024-10-11 12:00:43 +0300

    domcontent

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    const text = document.querySelector("p");
+    if(text){
+        text.textContent = "This is really cool!";
+    }
+
+});
+
+const text = document.querySelector("p");
+     if(text){
+        text.textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+     }
